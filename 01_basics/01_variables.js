d7524dc2a16f7e0a4b102fee2da50bf961635b90
Shyamal Ray <shyamalray89@gmail.com>
2024-05-06 05:50:51 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,26 @@
+const accountId = 144553
+let accountEmail = "shyamal.clawsys@gmail.com"
+
+var accountPassword = "12345"
+accountCity = "Bhubaneswar"
+let accountState;
+
+
+
+// accountId = 2 //not allowed
+
+// for(let index=0; index < array.length; index++){
+//     const element = array[index];
+// }
+
+accountEmail = "cotus.clawsys@gmail.com"
+accountPassword = "21212121"
+accountCity = "Bangalore"
+
+console.log(accountId);
+
+/*
+Prefer not to use var: because of issue in block scope & functional scope
+
+*/
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,21 @@
+"use strict"; //treat all JS as newer version
+
+// alert(3+3) //we are uisng nodeJS, not browser
+
+console.log("Hero");
+
+let name = "sam"
+let age = 34
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean=> true/false
+// null =>standalone value
+// undefined => 
+// symbol=> unique
+
+//object
+console.log(typeof undefined);
+console.log(typeof null);
